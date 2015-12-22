6aa1ef3abfb8088bb3613402d915eb3e9dd9619b
Vincent Salmiery <vince@contensive.com>
2015-12-22 09:13:16 -0500

    Update

--- COGR/passwordRecovery.vb
+++ COGR/passwordRecovery.vb
@@ -26,6 +26,10 @@ Namespace Contensive.Addons.COGR
                 Dim dbpassword As String = String.Empty
                 Dim loginok As Boolean = False
                 Dim cs As BaseClasses.CPCSBaseClass = CP.CSNew()
+                Dim emailBody As String = "Your username and password:<br>"
+                Dim passwordCnt As Integer = 0
+                Dim histCount As Integer = 0
+                Dim firstUserId As Integer = 0
 
                 If CP.User.IsRecognized And Not CP.User.IsAuthenticated Then
                     CP.User.Logout()
@@ -33,11 +37,48 @@ Namespace Contensive.Addons.COGR
                 End If
 
                 ' pull the information using the email
+                If cs.Open("people", "email = " & CP.Db.EncodeSQLText(email)) Then
+                    '
+                    'passwordCnt = CP.Doc.GetInteger("numberOfEmails")
 
-                CP.Email.sendPassword(email)
-                CP.Utils.AppendLog("cogrEmailLog.log", "Send Email To:" & email)
-               
+                    exitLoop = False
 
+                    'usernameTestCnt = 0
+                    Do
+                        '
+                        'histCount += 1
+                        If firstUserId = 0 Then
+                            firstUserId = cs.GetInteger("id")
+                        End If
+                        '
+                        Username = cs.GetText("userName")
+                        password = cs.GetText("password")
+                        '
+
+                        '
+                        emailBody &= "" _
+                         & "<br> User Name: " & Username _
+                         & "<br> Password: " & password _
+                         & "<br>" _
+                         & ""
+                        '
+                        'If passwordCnt <> 0 Then
+                        '    If histCount >= passwordCnt Then
+                        '        exitLoop = True
+                        '    End If
+                        'End If
+                        '
+
+                        Call cs.GoNext()
+                    Loop While cs.OK And (Not exitLoop)
+                        '
+                    CP.Email.sendSystem("Get Password", emailBody)
+                    CP.Email.send(email, "webmaster@COGR.edu", "This is your userName &amp; password", emailBody, True, True)
+                        'CP.Utils.AppendLog("cogrEmailLog.log", "Send Email To:" & email & "" & emailBody)
+                        '
+                        s = "OK"
+                End If
+                cs.Close()
 
 
                 returnHtml = "ok"
